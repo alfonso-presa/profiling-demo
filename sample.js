0243c162b22568f836878de0c7b293daf79d1d20
Alfonso Presa <alfonso.presa@gmail.com>
2016-09-26 19:59:57 +0200

    chore(): Added a simpler example for playing

---
+++ sample.js
@@ -0,0 +1,24 @@
+var webdriverio = require('webdriverio');
+var options = require('./test/config/pepino.conf');
+var Collector = require('./lib/collector');
+
+var collect = new Collector();
+var client = webdriverio.remote(options);
+
+client
+    .init()
+    .execute(':startProfile')
+    .url('http://localhost:8080')
+    //.execute(':endProfile').then(collect.cpu)
+    //.log('performance').then(collect.perf)
+    //.execute(':startProfile')
+    .click('button')
+    //.execute(':endProfile').then(collect.cpu)
+    //.log('performance').then(collect.perf)
+    //.execute(':startProfile')
+    .click('button')
+    .execute(':endProfile').then(collect.cpu)
+    .log('performance').then(collect.perf)
+    .catch(function (e) {
+    console.log(e);
+}).end();
