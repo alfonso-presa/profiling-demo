c7f28615120630ab3f3c6d784518c50a1cbe2e44
Alfonso Presa <alfonso.presa@gmail.com>
2016-09-27 12:32:07 +0200

    fix(): Not displaying methods

--- test/config/pepino.conf.js
+++ test/config/pepino.conf.js
@@ -12,14 +12,13 @@ var options = {
                 traceCategories:
                     'toplevel,'+
                     'ipc,'+
-                    '__metadata,'+
-                    'devtools.timeline,'+
-                    'disabled-by-default-devtools.timeline,'+
-                    'v8,'+
+                    // This breaks DevTools :-S => 'v8,'+
                     'blink,'+
                     'blink_gc,'+
                     'benchmark,'+
                     'latencyInfo,'+
+                    'devtools.timeline,'+
+                    'disabled-by-default-devtools.timeline,'+
                     'disabled-by-default-devtools.timeline.frame,'+
                     'disabled-by-default-cc.debug.display_items,'+
                     'disabled-by-default-cc.debug.picture,'+
